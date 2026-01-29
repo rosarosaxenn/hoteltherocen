513f5a2daa4c5a6ee140c06cdf0383efc328426c
rosa xennia <rosaxjparapat@gmail.com>
2026-01-29 14:15:48 +0000

    Edit koneksi.php

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -1,7 +1,15 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "hoteltherocen_db");
+// Mengambil data dari Environment Variables yang baru kamu input tadi
+$hostname = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$database = getenv('DB_NAME');
+$port     = 4000; // TiDB Cloud standarnya menggunakan port 4000
 
-if(!$koneksi){
-    die("koneksi gagal: " . mysqli_connect_error());
-}
+$conn = mysqli_connect($hostname, $username, $password, $database, $port);
+
+if (!$conn) {
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
+} 
+// Jika berhasil, variabel $conn siap digunakan untuk query!
 ?>
