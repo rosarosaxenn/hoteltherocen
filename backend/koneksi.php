048ed1ec081321551b92d343015da666c2da5add
rosa xennia <rosaxjparapat@gmail.com>
2026-06-13 22:51:57 +0000

    Edit koneksi.php

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -5,11 +5,11 @@ $password = getenv('DB_PASS');
 $database = getenv('DB_NAME');
 $port     = 4000;
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-mysqli_real_connect($conn, $hostname, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
+$koneksi = mysqli_init();
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($koneksi, $hostname, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
 
-if (!$conn) {
+if (!$koneksi) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
 ?>
