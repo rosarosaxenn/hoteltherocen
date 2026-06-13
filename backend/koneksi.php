4c9630913abc713569d891a4733df2c6939e49ac
rosa xennia <rosaxjparapat@gmail.com>
2026-06-13 22:45:41 +0000

    Edit koneksi.php

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -1,15 +1,15 @@
 <?php
-// Mengambil data dari Environment Variables yang baru kamu input tadi
 $hostname = getenv('DB_HOST');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASS');
 $database = getenv('DB_NAME');
-$port     = 4000; // TiDB Cloud standarnya menggunakan port 4000
+$port     = 4000;
 
-$conn = mysqli_connect($hostname, $username, $password, $database, $port);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($conn, $hostname, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
 
 if (!$conn) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
-} 
-// Jika berhasil, variabel $conn siap digunakan untuk query!
+}
 ?>
